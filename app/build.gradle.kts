3c29d907902f3acbe0bd16929a976dc641d1bc23
zmc <qazxcvfds123qiah@gmail.com>
2025-09-16 18:06:12 +0800

    feat: 自定义Ksp版AutoService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
-import org.gradle.kotlin.dsl.implementation
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
