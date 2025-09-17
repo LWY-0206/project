c5ff44e0b7b3f9b9c82b24dfd7f444682f795138
LWY-0206 <1184339646@qq.com>
2025-09-18 00:42:25 +0800

    合并清单声明注册

--- app/src/main/java/com/example/loding/app/MyApp.kt
+++ app/src/main/java/com/example/loding/app/MyApp.kt
@@ -1,13 +1,13 @@
 package com.example.loding.app
 
-import android.app.Application
 import android.util.Log
+import com.example.corekit.common.BaseApplication
 import com.jxdx.home.HomeModuleInitializer
 import com.jxdx.login.LoginModuleInitializer
 import com.jxdx.mine.service.MineModuleInitializer
 import com.jxdx.resource.ResourceModuleInitializer
 
-class MyApp : Application() {
+class MyApp : BaseApplication() {
     override fun onCreate() {
         super.onCreate()
         Log.d("---MyApp", "MyApp.onCreate() 执行了")
@@ -17,4 +17,12 @@ class MyApp : Application() {
         HomeModuleInitializer.init()
         ResourceModuleInitializer.init()
     }
+    companion object {
+        private const val BASE_URL = "http://121.41.176.238:8080/"
+        private const val ICON_FONT = "iconfont.ttf" // 字体文件路径
+    }
+
+    override fun httpBaseUrl(): String = BASE_URL
+
+    override fun iconFontPath(): String = ICON_FONT
 }
